a0cfd1ddafa89707da11d9cde9e8cdccd9535eb2
SceTeamHub <ybyns2760@gmail.com>
2024-12-22 04:54:09 +0100

    install.bat

--- install.bat
+++ install.bat
@@ -1,25 +1,18 @@
 @echo off
-title By SceHosting
+title Rickroll by SceHosting
 mode 1000,50
 color 0a
 setlocal enabledelayedexpansion
 
-set soundPath="%~dp0music.mp3"
+set soundPath="%~dp0scary_song.mp3"
 set boomMessage="BOOM! Your system is mine!"
-set rickRollURL="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
-for /l %%C in (1,1,5) do (
-    color 1f
-    timeout /t 1 >nul
-    color cf
-    timeout /t 1 >nul
-)
+set rickrollURL="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 if exist %soundPath% start "" %soundPath%
 
 cls
 echo SYSTEM FROZEN...
-color 1f
+color 0f
 timeout /t 2 >nul
 
 for /l %%C in (1,1,10) do (
@@ -29,72 +22,26 @@ for /l %%C in (1,1,10) do (
     timeout /t 0.5 >nul
 )
 
-for /l %%X in (1,1,5) do (
-    start cmd /c echo !boomMessage!
-    timeout /t 1 >nul
-)
-
 cls
 echo SYSTEM LOCKED - RESTARTING NOW...
-timeout /t 5 >nul
-
-cls
-echo A critical error occurred. Your system needs to shut down.
-timeout /t 5 >nul
-color 1f
-echo SYSTEM ERROR - RESTARTING NOW...
-timeout /t 5 >nul
-
-for /l %%A in (1,1,5) do (
-    color 0f
-    timeout /t 0.5 >nul
-    color 4f
-    timeout /t 0.5 >nul
-)
-
-cls
-echo SYSTEM INITIATING - DO NOT TURN OFF THE COMPUTER...
 timeout /t 3 >nul
 
 cls
-echo Booting up in safe mode...
+echo SYSTEM RESTARTING...
 timeout /t 2 >nul
-color 2f
-echo SYSTEM SECURE MODE ACTIVATED
-timeout /t 2 >nul
-
 cls
-echo Final check before launch...
+echo A critical error occurred. Your system needs to shut down.
 timeout /t 2 >nul
-color 5f
-echo SYSTEM LAUNCHING...
-timeout /t 2 >nul
-
-cls
-echo SYSTEM ONLINE - WELCOME BACK!
-timeout /t 2 >nul
-
-start "" "%rickRollURL%"
+color 0f
+echo SYSTEM ERROR - RESTARTING NOW...
 timeout /t 5 >nul
 
+if exist %soundPath% start "" %soundPath%
+
+start %rickrollURL%
+
 cls
-echo SYSTEM DISABLED - MOUSE INVERSION ACTIVATED
-timeout /t 2 >nul
-
-reg add "HKCU\Control Panel\Cursors" /v "MouseTrails" /t REG_DWORD /d 1 /f
-reg add "HKCU\Control Panel\Cursors" /v "Inverted" /t REG_DWORD /d 1 /f
-
-timeout /t 5 >nul
+echo SYSTEM LOCKED - STUCK IN RICKROLL
+timeout /t 999999 >nul
 
 exit
-
-:Animate
-set str=%~1
-set "temp="
-for /l %%A in (0,1,250) do (
-    set "temp=!temp!."
-    cls
-    echo %str%!temp!
-    timeout /t 0.1 >nul
-)
-goto :eof
